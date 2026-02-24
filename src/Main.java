338dcb2e81c8a913c6b11f7f8a12558ec581d334
Tanmay Singh <ts7583@srmist.edu.in>
2026-02-24 16:12:09 +0530

    UC1: Add Use Case 10 implementation

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static boolean isPalindrome(String str, int start, int end) {
+    public static boolean isPalindrome(String input) {
 
-        if (start >= end) {
-            return true;
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
         }
 
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-
-        return isPalindrome(str, start + 1, end - 1);
+        return true;
     }
 
     public static void main(String[] args) {
@@ -22,12 +27,10 @@ public class Main {
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        input = input.replaceAll("\\s+", "").toLowerCase();
-
-        boolean result = isPalindrome(input, 0, input.length() - 1);
+        boolean result = isPalindrome(input);
 
         if (result) {
-            System.out.println("The given string is a Palindrome.");
+            System.out.println("The given string is a Palindrome (ignoring spaces and case).");
         } else {
             System.out.println("The given string is NOT a Palindrome.");
         }
